5b508864a8cfcb43aa1265d65a8b87817daf8557
THUcqb <chenqibin422@gmail.com>
2017-11-30 13:49:23 +0800

    feat: complete randomly play chords when swiping
    Close #6

--- motion/swipehandler.py
+++ motion/swipehandler.py
@@ -1,4 +1,5 @@
 import Leap
+from sound import Player, GuitarChord
 
 class SwipeHandler(object):
 
@@ -9,4 +10,13 @@ class SwipeHandler(object):
         print('Swipe handler added.')
 
     def process(self, frame):
-        print('Swipe handler doing frame.')
+        swipe = None
+        for gesture in frame.gestures():
+            if gesture.type is Leap.Gesture.TYPE_SWIPE:
+                swipe = Leap.SwipeGesture(gesture)
+
+        if swipe is not None:
+            if swipe.state == Leap.SwipeGesture.STATE_START\
+            and Leap.Finger(swipe.pointable).type == Leap.Finger.TYPE_INDEX:
+                print('Got swipe gesture! z=%f' % swipe.position.z)
+                Player.play_notes(GuitarChord('random'))
