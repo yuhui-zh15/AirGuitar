f03e958a8bf4c938b21960ee0db81d586b7bfbb9
THUcqb <chenqibin422@gmail.com>
2017-12-12 20:35:27 +0800

    feat: make flask socketio app non-blocking
    Use eventlet.monkey_patch() and broadcast
    
    Relate #14

--- sound/socketplayer.py
+++ sound/socketplayer.py
@@ -1,10 +1,23 @@
+import eventlet
+eventlet.monkey_patch()
+
+from threading import Thread
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-if __name__ == '__main__':
+def start_server():
     socketio.run(app)
 
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+thread = Thread(target=start_server)
+if __name__ == '__main__':
+    thread.start()
+    while True:
+        socketio.sleep(1)
+        socketio.emit('strum', 1, broadcast=True)
--- ui/js/socket.js
+++ ui/js/socket.js
@@ -2,9 +2,9 @@ var socket = io.connect('http://localhost:5000');
 
 socket.on('connect', function() {
     socket.emit('my event', {data: 'I\'m connected!'});
-    vibrateString(1);
 });
 
-$(document).ready(function() {
-
+socket.on('strum', (stringId) => {
+    vibrateString(stringId);
 });
+
